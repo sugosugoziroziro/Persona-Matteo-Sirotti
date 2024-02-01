4d7db0e169f6d419ff3dd5e59a1612fd6d6b391d
SUGO <matte.sirotti2@gmail.com>
2024-02-01 16:46:01 +0100

    Add project files.

---
+++ Persona Matteo Sirotti/Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_Matteo_Sirotti
+{
+    internal class Persona
+    {
+        public string Nome { get; set; }
+        public int Eta { get; set; }
+        public Persona(string n, int e) 
+        {
+            Eta = e;
+            Nome = n;   
+        }
+        
+        public override string ToString() 
+        { 
+            return $"{Nome}";
+        }
+
+    }
+}
---
+++ Persona Matteo Sirotti/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona_Matteo_Sirotti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Persona> persone = new List<Persona>();
+            for(int i = 0;i<5;i++)
+            {
+                persone.Add(new Persona(Console.ReadLine(), int.Parse(Console.ReadLine())));
+               Text
+            }
+            for (int i = 0; i < persone.Count; i++)
+            {
+                if (persone[i].Eta>=18)
+                {
+                    Console.WriteLine(persone[i].ToString());
+                }
+            }
+            
+
+        }
+
+
+    }
+}
